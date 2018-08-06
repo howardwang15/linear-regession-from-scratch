ec2ae3d4ad56106e867e539a72cf58a7f1829882
howardwang15 <joy_luck_2000@hotmail.com>
2018-08-06 16:01:35 -0700

    added preprocessing to split data into training and testing

--- boston_houses.py
+++ boston_houses.py
@@ -11,14 +11,18 @@ def preprocess(boston_data, percentage):
     n_data = x.shape[0]
     n_training = int(percentage * n_data)
     x_train = np.empty(shape=(n_training, 13))
+    y_train = np.empty(shape=(n_training, 1))
     print(x_train.shape)
     for i in range(n_training):
         index = randint(0, n_data - 1)
         x_train[i] = x[index]
-        x = np.delete(x, index)
+        a = x[index]
+        y_train[i] = y[index]
+        x = np.delete(x, index, axis=0)
+        y = np.delete(y, index, axis=0)
         n_data -= 1
-    print(x_train)
-    print(x)
+    return x_train, x, y_train, y
+
 
 
 
@@ -28,4 +32,5 @@ def init_weights_and_bias():
 
 if __name__ == '__main__':
     boston = load_boston()
-    preprocess(boston, 0.67)
+    test = boston.keys()
+    x_train, x_test, y_train, y_test = preprocess(boston, 0.67)
