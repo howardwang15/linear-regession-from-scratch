6ddfe892097a0f6a560244ecfd6d3e547cb2512b
howardwang15 <joy_luck_2000@hotmail.com>
2018-08-06 18:41:47 -0700

    added mse

--- boston_houses.py
+++ boston_houses.py
@@ -1,8 +1,8 @@
 from sklearn.datasets import load_boston
 import numpy as np
 from random import randint
-import pandas as pd
 
+n_epochs = 500
 
 def preprocess(boston_data, percentage):
     x = boston_data.data
@@ -21,16 +21,30 @@ def preprocess(boston_data, percentage):
         x = np.delete(x, index, axis=0)
         y = np.delete(y, index, axis=0)
         n_data -= 1
-    return x_train, x, y_train, y
+    return x_train, x, y_train, y, n_training
 
 
+def init_weights_and_bias(training_size):
+    weights = np.random.rand(13, 1)
+    bias = 0.1
+    return weights, bias
 
 
-def init_weights_and_bias():
-    weights = np.array([1, 2])
+def mean_square_error(predicted, actual):
+    sum = 0
+    print(prediction.size)
+    for i in range(predicted.size):
+        error = np.square(actual[i] - predicted[i])
+        sum += error
+    return sum
 
 
 if __name__ == '__main__':
     boston = load_boston()
     test = boston.keys()
-    x_train, x_test, y_train, y_test = preprocess(boston, 0.67)
+    x_train, x_test, y_train, y_test, n_training = preprocess(boston, 0.67)
+    weights, bias = init_weights_and_bias(n_training)
+    for i in range(n_epochs):
+        prediction = np.matmul(x_train, weights) + bias
+        sum = mean_square_error(prediction, y_train)
+        
